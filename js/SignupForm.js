5075f587de1f0d684e555f81394e286370eaf92d
sunil kumar samal <sunilsamal322@gmail.com>
2022-12-25 20:35:40 +0530

    [Add]js for Signup form

---
+++ js/SignupForm.js
@@ -0,0 +1,8 @@
+document.getElementById('signup-form').addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let name=document.getElementById('name').value;
+    let email=document.getElementById('email').value;
+    let password=document.getElementById('password').value;
+    let phone=document.getElementById('phone').value;
+        
+})
