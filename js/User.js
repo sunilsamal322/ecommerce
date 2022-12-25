ec9b49cb0653b9a8cdc79a36da91c2ec4e583ece
sunil kumar samal <sunilsamal322@gmail.com>
2022-12-25 20:37:57 +0530

    [Add]Demo user data

---
+++ js/User.js
@@ -0,0 +1,18 @@
+let users=[
+    {
+        'id':1,
+        'username':'Sunil',
+        'password':'Sunil1234'
+    },
+    {
+        'id':2,
+        'username':'Akash',
+        'password':'Akash1234'
+    },
+    {
+        'id':3,
+        'username':'Rahul',
+        'password':'Rahul1234'
+    }
+
+]
